0c7d0124cd58a692d96ef1ad2a187651342c825d
GivX <romenzonez@gmail.com>
2024-02-18 01:47:37 +0700

    fix main page responsiveness

--- src/components/landingpage/HeroBanner.jsx
+++ src/components/landingpage/HeroBanner.jsx
@@ -11,11 +11,12 @@ const HeroBanner = () => {
       <main>
         <div className="pt-40 pb-40 flex content-center items-center justify-center">
           <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
+            className="absolute top-0 w-full h-[920px] bg-center bg-cover"
             style={{
               backgroundImage:
                 "url('https://i.postimg.cc/d0bQ4GGW/bg-hero.jpg')",
               backgroundAttachment: "fixed",
+
             }}
           >
             <span
@@ -33,7 +34,7 @@ const HeroBanner = () => {
                   Kami hadir untuk mendukung perjalanan dalam mengatasi masalah
                   stunting dan mencapai kesehatan bersama.
                 </p>
-                <div className="mt-8">
+                <div className="mt-8 absolute">
                   {!currentUser && (
                     <NavLink
                     to="/login"
